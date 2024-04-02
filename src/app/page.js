e38a3ca4b10c107cc4bb7c1f1994d0caed64a8a2
Gr34sy <slupek3@poczta.fm>
2024-04-02 18:57:30 +0200

    added background to html

---
+++ src/app/page.js
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main className="page">
+      <div>
+        <h1>
+          Strona w trakcie przebudowy! 
+          <span>Zapraszamy Niebawem</span>
+        </h1>
+      </div>
+    </main>
+  );
+}
