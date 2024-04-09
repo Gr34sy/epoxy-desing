a0711aa6f86815ea3be43779859366eac29341e7
Gr34sy <slupek3@poczta.fm>
2024-04-09 16:56:59 +0200

    added two lists with styling to navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,29 +1,58 @@
 "use client";
-import Image from "next/image";
-import Link from "next/link";
+// icons
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
+
+// logotype
 import logotype from "../../assets/images/logotype.png";
 
+// components
+import Image from "next/image";
+import Link from "next/link";
+
+// hooks
+import { useState } from "react";
+
 export function Navbar() {
+  const [showMobileNav, setShowMobileNav] = useState(false);
+
   return (
     <nav className="navbar">
+      {!showMobileNav && (
+        <FontAwesomeIcon icon={faBars} className="navbar__bars-icon" />
+      )}
+      {showMobileNav && (
+        <FontAwesomeIcon icon={faXmark} className="navbar__x-icon" />
+      )}
+
       <ul className="navbar__list">
-        <li className="navbar__list_item navbar__logo">
-          <Link href="/">
-            <Image
-              className="navbar__logotype"
-              src={logotype}
-              alt="Logotype of Epoxy Desing"
-              width={250}
-              height={50}
-            />
-          </Link>
+        <li className="navbar__list_item ">
+          <Link href="/">Strona Główna</Link>
         </li>
 
-        {/* <li className="navbar__list__item navbar__logo">
-                    <Link href="/">
-                        Test
-                    </Link>
-                </li> */}
+        <li className="navbar__list_item ">
+          <Link href="/">Produkty</Link>
+        </li>
+      </ul>
+
+      <Link href="/" className="navbar__logotype">
+        <Image
+          className="navbar__logotype_img"
+          src={logotype}
+          alt="Logotype of Epoxy Desing"
+          width={175}
+          height={35}
+        />
+      </Link>
+
+      <ul className="navbar__list">
+        <li className="navbar__list_item ">
+          <Link href="/">Zaprojektuj</Link>
+        </li>
+
+        <li className="navbar__list_item ">
+          <Link href="/">Kontakt</Link>
+        </li>
       </ul>
     </nav>
   );
