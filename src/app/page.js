a73f782822891e3a7ad2ade85287ec009cd71d6f
Piotr Słupski <piotr.slupski17@gmail.com>
2024-07-19 19:12:26 +0200

    testing next image component responsiveness

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,19 @@
+import dummyImg from '../assets/images/dummyDecoration.jpg';
 
+import Image from 'next/image';
 
 export default function Home() {
   return (
     <main className="homepage">
-      <div>
+      <div className='hero'>
         <h1 className="header">
           Strona w trakcie przebudowy! <br/>
           <span>Zapraszamy Niebawem</span>
         </h1>
+
+        <div className='hero__img-container'>
+          <Image src={dummyImg} fill alt="home decoration example" className='hero__img'/>
+        </div>
       </div>
     </main>
   );
