c61d2ef8584f1f3db45b7e2758d9982bc1400ce6
Piotr Słupski <piotr.slupski17@gmail.com>
2024-07-23 17:24:26 +0200

    added info to footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,19 @@
 import React from "react";
 
 export function Footer() {
-  return <footer>&copy; Piotr Słupski</footer>;
+  return (
+    <footer className="footer">
+      <div className="footer__container">
+        <p className="footer__copyright">&copy; Copyright Piotr Słupski</p>
+
+        <div>
+          <p>
+            Check out page author&apos;s{" "}
+            <a href="https://heroic-quokka-3e69bf.netlify.app">portfolio</a> and{" "}
+            <a href="https://github.com/Gr34sy">Github profile</a>
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
 }
