c33c10482770423d60f68b9fd6caf43a631d2d88
Gr34sy <slupek3@poczta.fm>
2024-12-29 17:07:21 +0100

    changed the description of navlinks

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,29 @@
-import { getMainPageDetails } from "@/lib/getMainPageDetails";
-import Hero from "@/components/HomepageComponents/Hero";
-import AboutUs from "@/components/HomepageComponents/AboutUs";
-import OrderCta from "@/components/HomepageComponents/OrderCta";
-import VideoSection from "@/components/HomepageComponents/VideoSection";
-import { getProducts } from "@/lib/getProducts";
+import { getMainPageDetails } from '@/lib/getMainPageDetails';
+import Hero from '@/components/HomepageComponents/Hero';
+import AboutUs from '@/components/HomepageComponents/AboutUs';
+import OrderCta from '@/components/HomepageComponents/OrderCta';
+import VideoSection from '@/components/HomepageComponents/VideoSection';
+import { getProducts } from '@/lib/getProducts';
+
+const placeholderProduct = {
+  name: '',
+  description: '',
+  price: 0,
+  imageUrl: '',
+  premium: false,
+  _id: '',
+};
+
+const placeholderDetails = {
+  title: 'Placeholder',
+  subtitle: 'Subtitle',
+  backgroundImg: '',
+};
 export default async function Home() {
-  const details = await getMainPageDetails();
-  const products = await getProducts();
+  const details = (await getMainPageDetails()) || placeholderDetails;
+
+  const products = (await getProducts()) || [placeholderProduct];
+
   const product = products[0];
 
   return (
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,17 +1,17 @@
-"use client";
+'use client';
 // icons
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons';
 
 // logotype
-import logotype from "../../assets/images/logotype.png";
+import logotype from '../../assets/images/logotype.png';
 
 // components
-import Image from "next/image";
-import Link from "next/link";
+import Image from 'next/image';
+import Link from 'next/link';
 
 // hooks
-import { useState } from "react";
+import { useState } from 'react';
 
 export function Navbar() {
   const [showMobileNav, setShowMobileNav] = useState(false);
@@ -20,8 +20,16 @@ export function Navbar() {
     <nav className="navbar">
       <FontAwesomeIcon
         icon={showMobileNav ? faXmark : faBars}
-        className={showMobileNav ? "fontawesome-icon navbar__bars-icon" : "fontawesome-icon navbar__x-icon"}
-        onClick={showMobileNav ? () => setShowMobileNav(false) : () => setShowMobileNav(true)}
+        className={
+          showMobileNav
+            ? 'fontawesome-icon navbar__bars-icon'
+            : 'fontawesome-icon navbar__x-icon'
+        }
+        onClick={
+          showMobileNav
+            ? () => setShowMobileNav(false)
+            : () => setShowMobileNav(true)
+        }
       />
 
       <ul className="navbar__list">
@@ -46,32 +54,42 @@ export function Navbar() {
 
       <ul className="navbar__list">
         <li className="navbar__list_item ">
-          <Link href="/design">Sprawdź Warianty</Link>
+          <Link href="/design">Warianty</Link>
         </li>
 
         <li className="navbar__list_item ">
-          <Link href="/order">Zamów</Link>
+          <Link href="/order">Zamówienie Specjalne</Link>
         </li>
       </ul>
 
       {/* MOBILE LIST */}
-      {showMobileNav && <ul className="navbar__list navbar__list--mobile">
-        <li className="navbar__list_item">
-          <Link href="/" onClick={() => setShowMobileNav(false)}>Strona Główna</Link>
-        </li>
+      {showMobileNav && (
+        <ul className="navbar__list navbar__list--mobile">
+          <li className="navbar__list_item">
+            <Link href="/" onClick={() => setShowMobileNav(false)}>
+              Strona Główna
+            </Link>
+          </li>
 
-        <li className="navbar__list_item">
-          <Link href="/products" onClick={() => setShowMobileNav(false)}>Produkty</Link>
-        </li>
+          <li className="navbar__list_item">
+            <Link href="/products" onClick={() => setShowMobileNav(false)}>
+              Produkty
+            </Link>
+          </li>
 
-        <li className="navbar__list_item ">
-          <Link href="/design" onClick={() => setShowMobileNav(false)}>Sprawdź Warianty</Link>
-        </li>
+          <li className="navbar__list_item ">
+            <Link href="/design" onClick={() => setShowMobileNav(false)}>
+              Warianty
+            </Link>
+          </li>
 
-        <li className="navbar__list_item ">
-          <Link href="/order" onClick={() => setShowMobileNav(false)}>Zamów</Link>
-        </li>
-      </ul>}
+          <li className="navbar__list_item ">
+            <Link href="/order" onClick={() => setShowMobileNav(false)}>
+              Zamówienie Specjalne
+            </Link>
+          </li>
+        </ul>
+      )}
     </nav>
   );
 }
